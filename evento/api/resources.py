83a1e49ae29975e7b29d1f89478c486c21d1590c
Linguagem de Programação Comercial <lpc.pmw@labin.canoas>
2017-04-24 21:47:32 -0300

    'v2'

--- evento/api/resources.py
+++ evento/api/resources.py
@@ -1,16 +1,18 @@
 from tastypie.resources import ModelResource
+from tastypie.authorization import Authorization
 from tastypie import fields, utils
-from evento.models import TipoInscricao, Inscricoes, PessoaFisica
+from evento.models import *
 from django.contrib.auth.models import User
 
 
 class TipoInscricaoResource(ModelResource):
     class Meta:
         queryset = TipoInscricao.objects.all()
-        allowed_methods = ['get']
+        allowed_methods = ['get', 'post', 'delete', 'put']
         filtering = {
-            "descricao": ('exact', 'startswith',)
+            "descricao": ('exact', 'startswith')
         }
+        authorization= Authorization()
 
 
 class UserResource(ModelResource):
@@ -24,10 +26,75 @@ class PessoaFisicaResource(ModelResource):
     class Meta:
         queryset = PessoaFisica.objects.all()
         allowed_methods = ['get']
+        filtering = {
+            "descricao": ('exact', 'startswith')
+        }
 
+class EventoResource(ModelResource):
+    class Meta:
+        queryset = Evento.objects.all()
+        allowed_methods = ['get']
+        filtering = {
+            "descricao": ('exact', 'startswith')
+        }
+
+class EventoCientificoResource(ModelResource):
+    class Meta:
+        queryset = EventoCientifico.objects.all()
+        allowed_methods = ['get']
+        filtering = {
+            "descricao": ('exact', 'startswith')
+        }
+
+class PessoaResource(ModelResource):
+    class Meta:
+        queryset = Pessoa.objects.all()
+        allowed_methods = ['get']
+        filtering = {
+            "descricao": ('exact', 'startswith')
+        }
+
+class PessoaJuridicaResource(ModelResource):
+    class Meta:
+        queryset = PessoaJuridica.objects.all()
+        allowed_methods = ['get']
+        filtering = {
+            "descricao": ('exact', 'startswith')
+        }
+
+class AutorResource(ModelResource):
+    class Meta:
+        queryset = Autor.objects.all()
+        allowed_methods = ['get']
+        filtering = {
+            "descricao": ('exact', 'startswith')
+        }
+
+class ArtigoCientificoResource(ModelResource):
+    evento = fields.ToOneField(EventoCientificoResource , 'evento')
+    class Meta:
+        queryset = ArtigoCientifico.objects.all()
+        allowed_methods = ['get']
+        filtering = {
+            "descricao": ('exact', 'startswith')
+        }
+
+class ArtigoAutorResource(ModelResource):
+    artigoCientifico = fields.ToOneField(ArtigoCientificoResource, 'artigoCientifico')
+    autor = fields.ToOneField(AutorResource, 'autor')
+    class Meta:
+        queryset = ArtigoAutor.objects.all()
+        allowed_methods = ['get']
+        filtering = {
+            "descricao": ('exact', 'startswith')
+        }
 
 class InscricaoResource(ModelResource):
     pessoa = fields.ToOneField(PessoaFisicaResource, 'pessoa')
+    evento = fields.ToOneField(Evento, 'evento')
     class Meta:
         queryset = Inscricoes.objects.all()
         allowed_methods = ['get']
+        filtering = {
+            "descricao": ('exact', 'startswith')
+        }
